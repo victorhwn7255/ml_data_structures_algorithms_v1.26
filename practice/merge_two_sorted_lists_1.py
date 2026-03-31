537eaef90af0d0e46a2f3de361778e688ae26bdf
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-04-01 06:56:04 +0800

    -[P] merge two sorted lists

---
+++ practice/merge_two_sorted_lists_1.py
@@ -0,0 +1,25 @@
+class ListNode:
+  def __init__(self, val=0, next=None):
+    self.val = val
+    self.next = next
+    
+def mergeTwoLists(list1: ListNode, list2:ListNode):
+  if list1 is None:
+    return list2
+  if list2 is None:
+    return list1
+  
+  head = ListNode(-300)
+  current = head
+  while list1 and list2:
+    if list1.val >= list2.val:
+      current.next = list2
+      list2 = list2.next
+    else:
+      current.next = list1
+      list1 = list1.next
+    current = current.next
+  
+  current.next = list1 if list1 else list2
+  
+  return head.next
