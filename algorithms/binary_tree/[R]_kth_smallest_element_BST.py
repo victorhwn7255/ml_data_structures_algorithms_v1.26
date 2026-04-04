5fb73e577063e935eb92e3b7605d5fcf56e58e02
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-04-05 07:08:21 +0800

    - [R] kth smallest element BST

---
+++ algorithms/binary_tree/[R]_kth_smallest_element_BST.py
@@ -0,0 +1,18 @@
+class TreeNode:
+  def __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+    
+def kthSmallest(root: TreeNode, k):
+  results = []
+  
+  def inorder(node: TreeNode):
+    if node is None:
+      return
+    inorder(node.left)
+    results.append(node.val)
+    inorder(node.right)
+  
+  inorder(root)
+  return results[k-1]
