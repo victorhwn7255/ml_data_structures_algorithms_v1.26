4ca690a0fc770a529e827e8d470624c5dfe1eee5
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-04-09 17:57:21 +0800

    - first bad version

---
+++ algorithms/sorting/first_bad_version.py
@@ -0,0 +1,20 @@
+BAD_VERSION = 4  # change this to test different cases
+
+def isBadVersion(version: int) -> bool:
+    return version >= BAD_VERSION
+
+def firstBadVersion(n: int):
+  left = 1
+  right = n
+  
+  while left <= right:
+    mid = (left + right) // 2
+    if isBadVersion(mid):
+      right = mid - 1
+    else:
+      left = mid + 1
+  
+  return left
+
+
+print(firstBadVersion(5))
