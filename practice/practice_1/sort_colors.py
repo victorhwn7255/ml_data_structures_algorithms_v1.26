97aa07c6150c79b2165cff780560fab9e87d271b
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-05-01 08:48:25 +0800

    - sort colors

---
+++ practice/practice_1/sort_colors.py
@@ -0,0 +1,17 @@
+def sortColors(nums: list[int]):
+  low = 0
+  mid = 0
+  high = len(nums) - 1
+  
+  while mid <= high:
+    if nums[mid] == 0:
+      nums[low], nums[mid] = nums[mid], nums[low]
+      low += 1
+      mid += 1
+    elif nums[mid] == 1:
+      mid += 1
+    else:
+      nums[mid], nums[high] = nums[high], nums[mid]
+      high -= 1
+
+  
