b534d713b0bd62474fe53d4340c1116e920412d1
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-05-04 07:57:25 +0800

    -practice

---
+++ practice/practice_1/intersection_of_two_linked_list.py
@@ -0,0 +1,22 @@
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+    
+def getIntersectionNode(headA: ListNode, headB: ListNode):
+  point_A = headA
+  point_B = headB
+  
+  while point_A is not point_B:
+    if point_A is None:
+      point_A = headB
+    else:
+      point_A = point_A.next
+    
+    if point_B is None:
+      point_B = headA
+    else:
+      point_B = point_B.next
+  
+  return point_A
+
