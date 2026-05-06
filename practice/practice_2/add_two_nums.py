c6babaaadfad0af275cab220ee5370edceef934c
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-05-07 06:40:02 +0800

    - practice

--- practice/practice_2/add_two_nums.py
+++ practice/practice_2/add_two_nums.py
@@ -4,6 +4,23 @@ class ListNode:
     self.next = next
     
 def addTwoNumbers(l1: ListNode, l2: ListNode):
+  dummy_head = ListNode()
+  current = dummy_head
+  carry = 0
   
-  return
+  while l1 or l2 or carry:
+    total_sum = carry
+    if l1:
+      total_sum += l1.val
+      l1 = l1.next
+    if l2:
+      total_sum += l2.val
+      l2 = l2.next
+    digit = total_sum % 10
+    carry = total_sum // 10
+
+    current.next = ListNode(val=digit)
+    current = current.next
+  
+  return dummy_head.next
 
