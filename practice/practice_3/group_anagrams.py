c1dcd14acb8ccac3db80926bfd6d6e70cf459a85
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-04 06:48:26 +0800

    - group anagrams practice

---
+++ practice/practice_3/group_anagrams.py
@@ -0,0 +1,14 @@
+def groupAnagrams(strs: list[str]):
+  results = {}
+  
+  for word in strs:
+    new_word = "".join(sorted(word))
+    if new_word in results:
+      results[new_word].append(word)
+    else:
+      results[new_word] = [word]
+  
+  return list(results.values())
+
+print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+
