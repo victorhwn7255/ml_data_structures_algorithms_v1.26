2933d5714df79495ac539a7160d4967f54ab1257
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-08 07:46:33 +0800

    - practice

---
+++ practice/practice_2/merge_sorted_array.py
@@ -0,0 +1,22 @@
+def merge(nums1: list[int], m: int, nums2: list[int], n: int):
+  p1 = m - 1
+  p2 = n - 1
+  current = m + n - 1
+  
+  while p1 >= 0 and p2 >= 0:
+    if nums2[p2] >= nums1[p1]:
+      nums1[current] = nums2[p2]
+      p2 -= 1
+    else:
+      nums1[current] = nums1[p1]
+      p1 -= 1
+    current -= 1
+    
+
+  while(p2 >= 0):
+    nums1[current] = nums2[p2]
+    p2 -= 1
+    current -= 1
+
+
+
