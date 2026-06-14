1f0a8946e961dfb55b51e27d8033c5d7d4447fab
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-15 07:50:54 +0800

    practice

---
+++ practice/practice_2/populating_next_right_pointers.py
@@ -0,0 +1,27 @@
+class Node:
+  def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+    self.val = val
+    self.left = left
+    self.right = right
+    self.next = next
+        
+from collections import deque
+
+def connect(root: Node):
+  if root is None:
+    return None
+  
+  queue = deque([root])
+  
+  while queue:
+    level_size = len(queue)
+    for i in range(level_size):
+      node = queue.popleft()
+      if i < level_size - 1:
+        node.next = queue[0]
+      if node.left:
+        queue.append(node.left)
+      if node.right:
+        queue.append(node.right)
+
+  return root
