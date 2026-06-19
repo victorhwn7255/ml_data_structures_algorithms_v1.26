cc1a24df75e9067728a953ec076eb48216869018
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-19 08:36:20 +0800

    - linked list cycle

---
+++ practice/practice_1/linked_list_cycle.py
@@ -0,0 +1,17 @@
+class ListNode:
+  def __init__(self, x):
+    self.val = x
+    self.next = None
+    
+def hasCycle(head: ListNode):
+  slow = head
+  fast = head
+  
+  while fast and fast.next:
+    slow = slow.next
+    fast = fast.next.next
+    
+    if slow == fast:
+      return True
+  
+  return False
