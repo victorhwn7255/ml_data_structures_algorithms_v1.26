c65491a9f2569ce885a6633c323e0b8e04e52e3c
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-23 07:32:41 +0800

    - 3 sum

---
+++ practice/practice_1/3_sum.py
@@ -0,0 +1,28 @@
+def threeSum(nums: list[int]):
+  nums.sort()
+  results = []
+  for i, num in enumerate(nums):
+    if i > 0 and nums[i] == nums[i-1]:
+      continue
+    left = i + 1
+    right = len(nums) - 1
+    target = 0 - num
+    while left < right:
+      if nums[left] + nums[right] > target:
+        right -= 1
+      elif nums[left] + nums[right] < target:
+        left += 1
+      else:
+        results.append([num, nums[left], nums[right]])
+        left += 1
+        right -= 1
+        
+        while left < right and nums[left] == nums[left-1]:
+          left += 1
+        while left < right and nums[right] == nums[right+1]:
+          right -= 1
+        
+  return results
+
+
+print(threeSum([-1,0,1,2,-1,-4]))
