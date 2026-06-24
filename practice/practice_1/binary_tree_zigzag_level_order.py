d7a18fad6a2962bcff3bf179428e92c6f16bfe89
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-06-25 07:37:41 +0800

    - practice

---
+++ practice/practice_1/binary_tree_zigzag_level_order.py
@@ -0,0 +1,34 @@
+class TreeNode:
+  def  __init__(self, val=0, left=None, right=None):
+    self.val = val
+    self.left = left
+    self.right = right
+
+from collections import deque
+ 
+def zigzagLevelOrder(root: TreeNode):
+  if root is None:
+    return []
+  
+  results = []
+  queue = deque([root])
+  left_to_right = 1
+  
+  while queue:
+    level_size = len(queue)
+    level = deque([])
+    for _ in range(level_size):
+      node = queue.popleft()
+      if node.left:
+        queue.append(node.left)
+      if node.right:
+        queue.append(node.right)
+      if left_to_right % 2 == 1:
+        level.append(node.val)
+      if left_to_right % 2 == 0:
+        level.appendleft(node.val)
+
+    results.append(level)
+    left_to_right += 1
+
+  return results
