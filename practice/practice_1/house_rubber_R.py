439834e56281dc2987cd76b44e57675ebc5ee1f7
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-07-01 07:09:03 +0800

    - practice

--- practice/practice_1/house_rubber_R.py
+++ practice/practice_1/house_rubber_R.py
@@ -1,4 +1,16 @@
 def rob(nums: list[int]):
+  if len(nums) == 1:
+    return nums[0]
   
+  dp = [0] * len(nums)
   
-  return 
+  dp[0] = nums[0]
+  dp[1] = max(nums[0], nums[1])
+  
+  for i in range(2, len(nums)):
+    dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+  
+  return dp[-1]
+
+
+print(rob([2,7,9,3,1]))
