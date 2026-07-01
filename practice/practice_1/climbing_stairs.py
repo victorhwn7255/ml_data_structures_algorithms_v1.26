f34ed88a364ba9fb2d8fe358cc1b79991216fc49
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-07-02 06:49:05 +0800

    - practice

---
+++ practice/practice_1/climbing_stairs.py
@@ -0,0 +1,14 @@
+def climbStairs(n: int):
+  dp = [0] * n
+  
+  if n == 1:
+    return 1
+  
+  dp[0] = 1
+  dp[1] = 2
+  
+  for i in range(2, n):
+    dp[i] = dp[i-1] + dp[i-2]
+
+  return dp[-1]
+
