0217c13200c4205a254a15761a40a8cc10de0fea
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-07-03 07:32:44 +0800

    - jump game

---
+++ practice/practice_1/jump_game.py
@@ -0,0 +1,11 @@
+def canJump(nums: list[int]):
+  dp = [False] * len(nums)
+  dp[0] = True
+  
+  for i in range(1, len(nums)):
+    for j in range(i):
+      if dp[j] and j + nums[j] >= i:
+        dp[i] = True
+        break
+  
+  return dp[-1]
