b01db89859cd1eb5fd6e0ec1b8fd4825510ed4b9
victorhwn7255 <victorheweinan725@yahoo.com.sg>
2026-07-13 08:28:31 +0800

    - max subarray

---
+++ practice/practice_1/maximum_subarray.py
@@ -0,0 +1,9 @@
+def maxSubArray(nums:list[int]):
+  current_sum = nums[0]
+  best_sum = nums[0]
+  
+  for num in nums[1:]:
+    current_sum = max(current_sum, current_sum+num)
+    best_sum = max(best_sum, current_sum)
+  
+  return best_sum
